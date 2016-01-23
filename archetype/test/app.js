e60c50c3905f3b2247b3a63235040e0137e395da
Travis Webb <me@traviswebb.com>
2016-01-23 02:04:48 -0500

    [archetype] generate test folder

---
+++ archetype/test/app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const _ = require('lodash')
+const smokesignals = require('smokesignals')
+
+module.exports = _.defaultsDeep({
+  pkg: {
+    name: require('../package').name + '-test'
+  },
+  api: {
+    models: { },
+    controllers: { },
+    services: { }
+  },
+  config: {
+    main: {
+      packs: [
+        smokesignals.Trailpack,
+        require('trailpack-core'),
+        require('../')
+      ]
+    },
+  }
+}, smokesignals.FailsafeConfig)
+
+
---
+++ archetype/test/trailpack.test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const assert = require('assert')
+
+describe('Trailpack', () => {
+  let pack
+  before(() => {
+    pack = global.app.packs.waterline
+  })
+  it('should be loaded into the app.packs collection', () => {
+    assert(pack)
+  })
+  describe('#validate', () => {
+    it.skip('TODO test')
+  })
+  describe('#configure', () => {
+    it.skip('TODO test')
+  })
+  describe('#initialize', () => {
+    it.skip('TODO test')
+  })
+})
